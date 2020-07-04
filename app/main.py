7ce04f880657b66ec43681f389ebe9dc0bf0acfc
Amon Silva <amon.bernardo@hotmail.com>
2020-07-04 20:53:56 -0300

    update caso o usuário entre com GET na rota de login

--- app/main.py
+++ app/main.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-@app.route('/login/', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
+@app.route('/login/', methods=['POST','GET'])
 def login():
     if request.method == 'POST':
         name = request.form['user']
         password = request.form["password"]
+
+        # Validação do autenticação com o banco dados
         return render_template('dashboard.html', name = name)
     else:
-        return render_template('index.html')
+        return render_template('index.html', msg="<p>Erro na autenticação, tente novamente.</p>")
 # FLASK_APP=hello.py
 # flask run --host=0.0.0.0
